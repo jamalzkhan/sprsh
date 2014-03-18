3bf6c24ee4194a92d32d472ef2ada12cb5a34f1d
Jamal Khan <me@jamalkhan.com>
2014-03-18 23:50:55 +0000

    Initial commit of spreadsheet app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('sprshApp', [
+  'ngCookies',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
